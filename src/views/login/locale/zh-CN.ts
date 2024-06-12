f469c98df60c9a70c282369e6edaa3015b02f110
Evant <rayshining12@163.com>
2024-06-12 19:23:17 +0800

    fix: 修改错字

--- src/views/login/locale/zh-CN.ts
+++ src/views/login/locale/zh-CN.ts
@@ -2,7 +2,7 @@ export default {
   'login.form.title': '用户认证',
   'login.form.username.errMsg': '用户名不能为空',
   'login.form.password.errMsg': '密码不能为空',
-  'login.form.login.errMsg': '登录出错，轻刷新重试',
+  'login.form.login.errMsg': '登录出错，请刷新重试',
   'login.form.login.success': '欢迎使用',
   'login.form.username.placeholder': '请输入用户名',
   'login.form.password.placeholder': '请输入密码',
