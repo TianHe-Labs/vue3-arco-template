72a48320fa0fcc600c933ea7749400ae2d1f0329
Evant <rayshining12@163.com>
2024-06-26 14:57:53 +0800

    style: 修复变量命名错误

--- uno.config.ts
+++ uno.config.ts
@@ -27,57 +27,58 @@ export default defineConfig({
       // ArcoDesign 变量
       // https://arco.design/vue/docs/token
       primary: {
-        DEFAULT: 'var(--primary-6)',
-        7: 'var(--primary-7)',
-        6: 'var(--primary-6)', // DEFAULT
-        5: 'var(--primary-5)',
-        4: 'var(--primary-4)',
-        3: 'var(--primary-3)',
-        2: 'var(--primary-2)',
-        1: 'var(--primary-1)',
+        DEFAULT: 'rgba(var(--primary-6))',
+        7: 'rgba(var(--primary-7))',
+        6: 'rgba(var(--primary-6))', // DEFAULT
+        5: 'rgba(var(--primary-5))',
+        4: 'rgba(var(--primary-4))',
+        3: 'rgba(var(--primary-3))',
+        2: 'rgba(var(--primary-2))',
+        1: 'rgba(var(--primary-1))',
       },
 
       success: {
-        DEFAULT: 'var(--success-6)',
-        7: 'var(--success-7)',
-        6: 'var(--success-6)', // DEFAULT
-        5: 'var(--success-5)',
-        4: 'var(--success-4)',
-        3: 'var(--success-3)',
-        2: 'var(--success-2)',
-        1: 'var(--success-1)',
+        DEFAULT: 'rgba(var(--success-6))',
+        7: 'rgba(var(--success-7))',
+        6: 'rgba(var(--success-6))', // DEFAULT
+        5: 'rgba(var(--success-5))',
+        4: 'rgba(var(--success-4))',
+        3: 'rgba(var(--success-3))',
+        2: 'rgba(var(--success-2))',
+        1: 'rgba(var(--success-1))',
       },
 
       warning: {
-        DEFAULT: 'var(--warning-6)',
-        7: 'var(--warning-7)',
-        6: 'var(--warning-6)', // DEFAULT
-        5: 'var(--warning-5)',
-        4: 'var(--warning-4)',
-        3: 'var(--warning-3)',
-        2: 'var(--warning-2)',
-        1: 'var(--warning-1)',
+        DEFAULT: 'rgba(var(--warning-6))',
+        7: 'rgba(var(--warning-7))',
+        6: 'rgba(var(--warning-6))', // DEFAULT
+        5: 'rgba(var(--warning-5))',
+        4: 'rgba(var(--warning-4))',
+        3: 'rgba(var(--warning-3))',
+        2: 'rgba(var(--warning-2))',
+        1: 'rgba(var(--warning-1))',
       },
 
       danger: {
-        DEFAULT: 'var(--danger-6)',
-        7: 'var(--danger-7)',
-        6: 'var(--danger-6)', // DEFAULT
-        5: 'var(--danger-5)',
-        4: 'var(--danger-4)',
-        3: 'var(--danger-3)',
-        2: 'var(--danger-2)',
-        1: 'var(--danger-1)',
+        DEFAULT: 'rgba(var(--danger-6))',
+        7: 'rgba(var(--danger-7))',
+        6: 'rgba(var(--danger-6))', // DEFAULT
+        5: 'rgba(var(--danger-5))',
+        4: 'rgba(var(--danger-4))',
+        3: 'rgba(var(--danger-3))',
+        2: 'rgba(var(--danger-2))',
+        1: 'rgba(var(--danger-1))',
       },
+
       // ArcoDesign 链接
       link: {
-        7: 'var(--link-7)',
-        6: 'var(--link-6)',
-        5: 'var(--link-5)',
-        4: 'var(--link-4)',
-        3: 'var(--link-3)',
-        2: 'var(--link-2)',
-        1: 'var(--link-1)',
+        7: 'rgba(var(--link-7))',
+        6: 'rgba(var(--link-6))',
+        5: 'rgba(var(--link-5))',
+        4: 'rgba(var(--link-4))',
+        3: 'rgba(var(--link-3))',
+        2: 'rgba(var(--link-2))',
+        1: 'rgba(var(--link-1))',
       },
       // ArcoDesign 文字
       text: {
