3401956958b773e00274e5b6c258b33218e3394e
Dllay <rayshining12@163.com>
2025-04-16 17:31:40 +0800

    feat: 工具函数

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -59,3 +59,19 @@ export function isPhone(obj: any): boolean {
 export function isEmail(obj: any): boolean {
   return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(obj);
 }
+
+export function isIp(obj: any): boolean {
+  return isIpv4(obj) || isIpv6(obj);
+}
+
+export function isIpv4(obj: any): boolean {
+  return /^(\d{1,3}\.){3}\d{1,3}$/.test(obj);
+}
+
+export function isIpv6(obj: any): boolean {
+  return /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/.test(obj);
+}
+
+export function isMac(obj: any): boolean {
+  return /^([0-9a-fA-F]{2}-){5}[0-9a-fA-F]{2}$/.test(obj);
+}
